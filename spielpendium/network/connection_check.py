0ef3c7babd4e1f946cc2cadfc07f46974613976c
Eddie Ruiz <eduardo.j.ruiz@gmail.com>
2021-08-01 01:05:04 -0400

    [#19] Filled out function to check BGG API status. Edited info_grabber.py functions to only use logger if it's not explicitly passed (if logger=None is passed, no logging will be done). This is so the BGG API checking function doesn't log anything, since it won't be a user-initiated action.

--- spielpendium/network/connection_check.py
+++ spielpendium/network/connection_check.py
@@ -2,17 +2,20 @@ import socket
 import urllib.request
 import urllib.error
 
+from spielpendium.network import search_bgg
 
-_BGG_URL = "https://www.boardgamegeek.com"
+__all__ = ['is_connected_to_internet', 'bgg_is_up', 'bgg_api_is_up']
+
+_BGG_URL = "https://www.boardgamegeek.com/"
 
 
 def is_connected_to_internet() -> bool:
     """Checks if there's an internet connection
     
-    :returns: True if an internet connection exists, False otherwise.
+    :return: True if an internet connection exists, False otherwise.
     """
     try:
-        # try to connect to 1.1.1.1, which is a DNS server and should always be up
+        # try to connect to 1.1.1.1, a DNS server that should always be up
         socket.create_connection(("1.1.1.1", 53))
         return True
     except OSError:
@@ -24,24 +27,38 @@ def is_connected_to_internet() -> bool:
 def bgg_is_up() -> bool:
     """Checks if boardgamegeek.com is up
 
-    :returns: True if boardgamegeek.com is up, False otherwise.
-	"""
+    :return: True if boardgamegeek.com is up, False otherwise.
+    """
 
     try:
-        # try to connect to.BGG and check the return status. 200 means "ok success status"
+        # try to connect to.BGG and check the return status.
+        # 200 means "ok success status"
         return urllib.request.urlopen(_BGG_URL).getcode() == 200
     except urllib.error.URLError:
         # if there's any error with connecting, return False
         pass
     
     return False
-    
-    
-    def bgg_api_is_up() -> bool:
-    	
-    	return True
+
+
+def bgg_api_is_up() -> bool:
+    """
+
+    :return: True if the BGG API is working, False otherwise
+    """
+
+    try:
+        # Try a test search using the BGG API. It is works, the API is up
+        search_bgg('Catan', logger=None)
+        return True
+    except urllib.error.HTTPError:
+        # If it doesn't work, the API is down
+        pass
+
+    return False
+
 
 if __name__ == '__main__':
     print(is_connected_to_internet())
     print(bgg_is_up())
-	
+    print(bgg_api_is_up())
