dffdf2314bcfb0410822bb864a32d0557114b880
Eduardo Ruiz <eduardo.j.ruiz@gmail.com>
2021-07-31 21:12:10 -0400

    Adding function to check internet connection status.

---
+++ spielpendium/network/connection_check.py
@@ -0,0 +1,20 @@
+import socket
+
+
+def is_connected():
+    """Checks if there's an internet connection
+    
+    :returns: True if an internet connection exists, False otherwise.
+    """
+    try:
+        # try to connect to 1.1.1.1, which should always be up
+        socket.create_connection(("1.1.1.1", 53))
+        return True
+    except OSError:
+        # if it can't connect, it'll come here
+        pass
+    return False
+
+
+if __name__ == '__main__':
+	print(is_connected())
