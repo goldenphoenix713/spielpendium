08af109a6f4d78a2a27e69fe10fc40bd2f054fd1
Eduardo Ruiz <eduardo.j.ruiz@gmail.com>
2021-08-03 07:42:35 -0400

    Adding more info to Games stub.

--- spielpendium/data/games.py
+++ spielpendium/data/games.py
@@ -10,12 +10,36 @@ class Games(QtCore.QAbstractTableModel):
     def __init__(self, parent=None):
         super(Games, self).__init__(parent)
         
-        self._games = []
+        # TODO Add more column names
+        column_names = [
+            'bgg_id',
+            'game_name',
+            'game_subname',
+            'author',
+            'artist',
+            'publisher',
+            'category',
+            'image',
+            'description',
+            'min_players',
+            'max_players',
+            'recommended_players',
+            'age',
+            'complexity',
+        ]
+        
+        self._games = pd.DataFrame(columns=column_names)
     
     def load(self, filename: str) -> bool:
         pass
     
     def save(self, filename: str) -> bool:
+        pass
+    
+    def read_db(self) -> bool:
+        pass
+     
+    def write_db(self) -> bool:
         pass
     
 
