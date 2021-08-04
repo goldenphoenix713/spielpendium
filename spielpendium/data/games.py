5df18c8b4dea65ae19a692a29a56edb8a55e0a75
Eduardo Ruiz <eduardo.j.ruiz@gmail.com>
2021-08-03 22:30:56 -0400

    Implemented rowCount, columnCount, and index methods.

--- spielpendium/data/games.py
+++ spielpendium/data/games.py
@@ -11,13 +11,15 @@ class Games(QtCore.QAbstractTableModel):
         super(Games, self).__init__(parent)
         
         # TODO Add more column names
-        column_names = [
+        self._header = [
             'bgg_id',
             'game_name',
             'game_subname',
+            'version',
             'author',
             'artist',
             'publisher',
+            'release_year',
             'category',
             'image',
             'description',
@@ -25,10 +27,28 @@ class Games(QtCore.QAbstractTableModel):
             'max_players',
             'recommended_players',
             'age',
+            'min_play_time',
+            'max_play_time',
+            'bgg_rating',
+            'bgg_rank',
             'complexity',
+            'related_games',
         ]
         
-        self._games = pd.DataFrame(columns=column_names)
+        self._games = pd.DataFrame(columns=self._header)
+    
+    def rowCount(self, parent=QtCore.QModelIndex()) -> int:
+        return len(self._games)
+    
+    def columnCount(self, parent=QtCore.QModelIndex()) -> int:
+        return len(self._games.columns)
+    
+    def data(self, index, role):
+        pass
+    
+    def index(self, row, column, parent=QtCore.QModelIndex()) -> QtCore.QModelIndex:
+        return self.createIndex(row, column)
+        
     
     def load(self, filename: str) -> bool:
         pass
